d6883044defca51a257a1a069d9634f220e7b2b8
MarianaMTF <142687755+MarianaMTF@users.noreply.github.com>
2023-10-18 19:52:22 -0300

    Create atividade5.py

---
+++ atividade5.py
@@ -0,0 +1,9 @@
+lista=[]
+quantidade=int(input("digite a quantidade de numeros:"))
+for i in range(0, (quantidade+1)):
+  lista.append(int(input(("digite um numero:")))
+maior=0
+for num in lista:
+  if num > maior:
+    maior=num
+print(f"o maior numero é {maior}")
